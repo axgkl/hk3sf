28d6415635b8e6d658e3a2c000f16fdac33bf8e9
Gunther Klessinger <gunther.klessinger@axiros.com>
2024-08-11 18:05:23 +0200

    ci test :secrets:setup:

--- tests/test_secrets.sh
+++ tests/test_secrets.sh
@@ -7,8 +7,6 @@ bash -c '. ./main.sh servers .id' # standalone call (only in bash)
 
 . ./main.sh import
 test "$(servers)" == "$(. ./main.sh servers)"
-
-echo "$DOMAIN"
-echo "xxx$DOMAIN"
 digitalocean_dns_list | jq .id
+
 echo success
--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -1,1 +1,27 @@
-echo testingsecrets
+#!/usr/bin/env bash
+
+NAME="citest"
+FN_SSH_KEY="ssh_key"
+HOST_NETWORK=2
+LOCATION="hel1"
+DNS_PROVIDER="digitalocean"
+
+source "./main.sh" import
+
+ensure_local_ssh_key # call, when $FN_SSH_KEY possibly not present
+check_requirements
+#images
+#ssh_keys
+#ensure_ip_forwarder "37.27.193.230"
+#postinstall "37.27.193.230"
+ensure_proxy_server
+#postinstall "37.27.193.230"
+ensure_k3s_via_proxy
+ensure_proxy_is_loadbalancer
+enable_local_kubectl
+
+ensure_ingress_nginx_with_certmgr
+
+# test_autoscale
+report
+#
