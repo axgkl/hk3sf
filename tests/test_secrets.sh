7805a691bdc897a2775b1e3455e651e6640b9bd7
Gunther Klessinger <gunther.klessinger@axiros.com>
2024-08-12 15:42:46 +0200

    ci test :setup:keep:

--- tests/test_secrets.sh
+++ tests/test_secrets.sh
@@ -6,8 +6,8 @@ trap 'echo "Error on line $LINENO $BASH_COMMAND"' ERR
 bash -c '. ./main.sh servers .id' # standalone call (only in bash)
 
 . ./main.sh
-test "$(servers)" == "$(. ./main.sh servers)"
-digitalocean_dns_list | jq .id
+test "$(shw servers)" == "$(. ./main.sh servers)"
+shw digitalocean_dns_list | jq .id
 
 echo success
 # set -x
