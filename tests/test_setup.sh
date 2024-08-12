5eee7712c8adcaac2f4dee226fb51d24ba181aec
Gunther Klessinger <gunther.klessinger@axiros.com>
2024-08-12 16:20:50 +0200

    ci test :setup:keep:

--- tests/test_setup.sh
+++ tests/test_setup.sh
@@ -3,12 +3,12 @@ test -z "${GITHUB_ACTIONS:-}" && . tests/environ # local testing
 trap 'echo "Error on line $LINENO $BASH_COMMAND"' ERR
 
 source "./main.sh" "$@"
-report && echo done
-exit 2
+report && echo donereport
 ensure_requirements
 ensure_proxy_server
 ensure_k3s_via_proxy
 ensure_proxy_is_loadbalancer
 enable_local_kubectl
 ensure_ingress_nginx_with_certmgr
-report
+report && echo reported
+echo foo
