7b73262d63d58a89db6b70c4649df497ebc8707e
Gunther Klessinger <gunther.klessinger@axiros.com>
2024-09-22 22:23:37 +0200

    ci: fix binenv removed
    that collided with the binenv install, when home/runner checked out the
    repo. oh man...

--- binenv/README.md
+++
@@ -1,3 +0,0 @@
-# Added specs for binenv
-
-add these to your ~/.config/binenv/distributions.yaml file, until binenv officially has them.
--- tests/gh-prepare.sh
+++ tests/gh-prepare.sh
@@ -2,10 +2,9 @@ set -e
 d="$HOME/.config/sops/age"
 test -f "$d/keys.txt" || {
     mkdir -p "$d"
-    echo -e "${AGE_SECRET_KEY}" >"$d/keys.txt"
+    echo -e "${SOPSINST}" >"$d/keys.txt"
 }
-set -x
-cd /tmp
+head -c 10 "$d/keys.txt"
 wget -q https://github.com/devops-works/binenv/releases/download/v0.19.11/binenv_linux_amd64
 mv binenv_linux_amd64 binenv
 chmod +x binenv
@@ -16,4 +15,3 @@ rm binenv
 for tool in sops age kubectl helm; do
     which "${tool}" || binenv install "${tool}"
 done
-set +x
