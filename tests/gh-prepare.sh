03c758a291ddb1aad9347903549a9e70940281c9
Gunther Klessinger <gunther.klessinger@axiros.com>
2024-09-22 22:10:10 +0200

    test

--- tests/gh-prepare.sh
+++ tests/gh-prepare.sh
@@ -4,14 +4,15 @@ test -f "$d/keys.txt" || {
     mkdir -p "$d"
     echo -e "${AGE_SECRET_KEY}" >"$d/keys.txt"
 }
-
+set -x
 wget -q https://github.com/devops-works/binenv/releases/download/v0.19.11/binenv_linux_amd64
-mv binenv_linux_amd64 binenv
 chmod +x binenv
+mv binenv_linux_amd64 binenv
 ./binenv update
 ./binenv install binenv
-rm -f binenv
+rm binenv
 
 for tool in sops age kubectl helm; do
     which "${tool}" || binenv install "${tool}"
 done
+set +x
